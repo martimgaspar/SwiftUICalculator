b36005171265c90fe0c8ddcd37ce5ed34f13142c
martimgaspar <martimgaspar@gmail.com>
2023-09-15 12:54:47 -0400

    Initial Commit

---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Martim Gaspar on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
